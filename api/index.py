ac5baf8f88e1c120ec761c58bf06637ae62345b2
arneshbanerjeedgp-gif <arneshbanerjeedgp@gmail.com>
2026-06-11 13:19:27 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -10,7 +10,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://marksmind.in", "http://localhost:3000"],
+    allow_origins=["*"],  # This allows Google AI Studio to successfully connect!
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
